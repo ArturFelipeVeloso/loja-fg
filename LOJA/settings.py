d202c8b5b8cafaee346fcdcc28aea8e488072481
ArturFelipeVeloso <arturfdasveloso@gmail.com>
2019-10-26 02:16:33 -0300

    AGORA VAI

--- LOJA/settings.py
+++ LOJA/settings.py
@@ -127,7 +127,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-MEDIA_URL = '/media/'
 
 if DEBUG:
     STATICFILES_DIRS = (
@@ -136,6 +135,18 @@ if DEBUG:
 else:
     STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+# HEROKU
+abspath = lambda *p: os.path.abspath(os.path.join(*p))
+
+PROJECT_ROOT = abspath(os.path.dirname(__file__))
+
+MEDIA_ROOT = abspath(PROJECT_ROOT, 'media')
+MEDIA_URL = '/media/'
+# HEROKU
+
+#MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+#MEDIA_URL = '/media/'
+
 
 # AGORA VAI
